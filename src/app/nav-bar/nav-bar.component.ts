2979fbf902e8468343762d7d0486d43287405356
André Rosa (Vivobook) <andre.reddrain@gmail.com>
2026-01-14 17:12:37 +0000

    Routes na NavBar.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { Menubar } from 'primeng/menubar';
 import { BadgeModule } from 'primeng/badge';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [Menubar, BadgeModule],
+  imports: [Menubar, BadgeModule, RouterLink],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
