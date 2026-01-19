675736d8503547139821c0d439ed2ac84d68ecab
André Rosa (Vivobook) <andre.reddrain@gmail.com>
2026-01-19 00:35:16 +0000

    Começo dos botões das Raids.

--- src/app/pages/raids/raids.component.ts
+++ src/app/pages/raids/raids.component.ts
@@ -1,12 +1,30 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+interface Raid {
+  name: string
+}
 
 @Component({
   selector: 'app-raids',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, NgFor],
   templateUrl: './raids.component.html',
   styleUrl: './raids.component.scss'
 })
 export class RaidsComponent {
+  raids: Raid[] | undefined;
 
+  ngOnInit() {
+    this.raids = [
+      { name: 'Argos' },
+      { name: 'Thaemine' },
+      { name: 'Valtan' },
+      { name: 'Vykas' },
+      { name: 'Brelshaza' },
+      { name: 'Aegir' },
+      // More sidebar items here...
+    ]
+  }
 }
