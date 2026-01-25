fbbae952d02da4ed0d08a4165d8ca2119140c940
André Rosa (Vivobook) <andre.reddrain@gmail.com>
2026-01-25 16:47:27 +0000

    Modificação do formulário de Login/Register.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,12 +2,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { FormsModule } from "@angular/forms";
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ButtonModule, DialogModule, FormsModule, NgIf],
+  imports: [InputTextModule, ButtonModule, DialogModule, FormsModule, NgIf, NgClass],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -18,20 +20,107 @@ export class LoginComponent {
   mode: 'login' | 'register' = 'login';
 
   // Sign Up Variables
-  username = '';
-  email = '';
-  password = '';
-  confirmPassword = '';
+  username: string | null = null;
+  email: string | null = null;
+  password: string | null = null;
+  confirmPassword: string | null = null;
 
+  usernameTouched = false;
+  emailTouched = false;
+  passwordTouched = false;
+  confirmPasswordTouched = false;
+
+  // TODO Adicionar mais verificações!
+  /**
+   * Getter to validate Username
+   */
+  get isUsernameInvalid(): boolean {
+    if (!this.username || this.username.trim().length === 0) return true;
+
+    return false;
+  }
+
+  /**
+   * Getter to validate Email
+   */
+  get isEmailInvalid(): boolean {
+    if (!this.email || this.email.trim().length === 0) return true;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return !emailRegex.test(this.email.trim());
+  }
+
+  /**
+   * Getter to validate Password
+   */
+  get isPasswordInvalid(): boolean {
+    if (!this.password || this.password.trim().length === 0) return true;
+
+    if (this.mode === 'register' && this.password !== this.confirmPassword) return true;
+
+    return false;
+  }
+
+  /**
+   * Getter to validate if User can submit Login / Register
+   */
+  get canSubmit(): boolean {
+    // Login
+    if (this.mode === 'login') {
+      return (
+        !this.isEmailInvalid && !this.isPasswordInvalid
+      );
+    }
+
+    // Register
+    return (
+      !this.isEmailInvalid && !this.isUsernameInvalid && !this.isPasswordInvalid
+    )
+  }
+
+  onUsernameChange(value: string | null) {
+    this.username = value;
+
+    if (value && value.trim().length > 0) this.usernameTouched = true;
+    else this.usernameTouched = false;
+  }
+
+  onEmailChange(value: string | null) {
+    this.email = value;
+
+    if (value && value.trim().length > 0) this.emailTouched = true;
+    else this.emailTouched = false;
+  }
+
+  onPasswordChange(value: string | null) {
+    this.password = value;
+
+    if (value && value.trim().length > 0) this.passwordTouched = true;
+    else this.passwordTouched = false;
+  }
+
+  onConfirmPasswordChange(value: string | null) {
+    this.confirmPassword = value;
+
+    if (value && value.trim().length > 0) this.confirmPasswordTouched = true;
+    else this.confirmPasswordTouched = false;
+  }
+
+  /**
+   * Swaps mode from Login <-> Register
+   */
   toggleMode() {
     this.mode = this.mode === 'login' ? 'register' : 'login';
   }
 
+  /**
+   * Submits the user's input depending on the mode
+   */
   submit() {
     if (this.mode === 'login') {
-      // TODO Login logic
+      console.log("Vai fazer o login!");
     } else {
-      // TODO Register logic
+      console.log("Vai fazer o register!");
     }
     this.close();
   }
