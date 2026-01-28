f843b31a8a26cc8bcfe2f4afbbc8b8dc3e7fcea6
André Rosa (Vivobook) <andre.reddrain@gmail.com>
2026-01-28 17:41:21 +0000

    Estilização de um character.

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -2,13 +2,23 @@ import { Component } from '@angular/core';
 import { CharacterService } from '../../services/character/character.service';
 import { CharacterClassesService } from '../../services/character-classes/character-classes.service';
 import { AuthService } from '../../services/auth/auth.service';
+import { Toast } from "primeng/toast";
+import { NgFor } from '@angular/common';
+import { MessageService } from 'primeng/api';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { Tooltip } from "primeng/tooltip";
+import { InputText } from "primeng/inputtext";
+import { InputNumberModule } from 'primeng/inputnumber';
+import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   selector: 'app-characters',
   standalone: true,
-  imports: [],
+  imports: [TextareaModule, InputNumberModule, ButtonModule, FormsModule, Toast, NgFor, Tooltip, InputText],
   templateUrl: './characters.component.html',
-  styleUrl: './characters.component.scss'
+  styleUrl: './characters.component.scss',
+  providers: [MessageService]
 })
 export class CharactersComponent {
   // Create Character Form
@@ -34,23 +44,24 @@ export class CharactersComponent {
     // Load of CharacterClasses
     this.characterClassesService.getAllCharacterClasses().subscribe((data: any) => {
       this.characterClasses = data;
-      console.log(this.characterClasses)
-    })
 
-    // Load of User's Characters
-    this.characterService.getCharactersOfUser().subscribe((data: any) => {
-      this.characters = data;
-      console.log(this.characters);
+      // Load of User's Characters
+      this.listCharacters();
     })
   }
 
   listCharacters() {
     this.characterService.getCharactersOfUser().subscribe({
       next: (data: any) => {
-        console.log(data)
+        const charactersData = data.map((character: any) => ({
+          ...character,
+          characterClass: this.characterClasses.find((cc: { id: any; }) => cc.id === character.characterClassId)
+        }))
+        this.characters = charactersData;
+        console.log(this.characters);
       },
       error: err => {
-        console.error(err)
+        console.error(err);
       }
     })
   }
@@ -72,5 +83,13 @@ export class CharactersComponent {
         // Create error!
       }
     })
+  }
+
+  updateCharacter(character: any) {
+
+  }
+
+  deleteCharacter(character: any) {
+
   }
 }
